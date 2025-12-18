1c11695bbfc410a366029b9d538151dd70d67b6c
sushant babu prasai <sushantprasaii7@gmail.com>
2025-12-18 21:54:01 +0545

    Init: initial setup of frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1>Welcome to leadXpert frontend!</h1>
+    </div>
+  );
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express()
+
 const port = 5500;
 
 app.listen(port, () => {
