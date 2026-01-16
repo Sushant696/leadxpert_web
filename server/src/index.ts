bdceee90000dd97719124e20ae008f51bfa75a1d
sushant babu prasai <sushantprasaii7@gmail.com>
2026-01-16 11:50:50 +0545

    Refactor: shifting from mono-repo to multi-repo

--- server/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { env } from "./config/env";
-import { logger } from "./lib/logger";
-import app from "./server";
-
-app.listen(env.PORT || 5500, () => {
-  logger.info("Server Started on port : " + env.PORT)
-})
